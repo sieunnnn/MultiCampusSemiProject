43583ed33c7e40fafec3abecbe5d3135c010a2fe
시은 <119668620+sieunnnn@users.noreply.github.com>
2023-02-13 10:52:55 +0900

    remove personal information

--- spring_todolist/spring_todolist/src/main/java/com/mc/mvc/common/code/Code.java
+++ spring_todolist/spring_todolist/src/main/java/com/mc/mvc/common/code/Code.java
@@ -3,7 +3,7 @@ package com.mc.mvc.common.code;
 public enum Code {
 	
 	DOMAIN("http://localhost:8080"),
-	SMTP_FROM("rkqtl_@naver.com"),
+	SMTP_FROM(""),
 	
 	// 배포시 servlet-context.xml의 resources 경로도 함께 수정
 	STORAGE_PATH("E:\\Program Files\\CODE\\storage\\");
